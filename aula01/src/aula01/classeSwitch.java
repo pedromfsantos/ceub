cd9deb8ae51ee2fc9543f8aff24f10972c0623c9
Pedro Matheus <pedro.matheusfs@gmail.com>
2024-05-10 12:31:51 -0300

    aquele switch

---
+++ aula01/src/aula01/classeSwitch.java
@@ -0,0 +1,11 @@
+package aula01;
+
+public class classeSwitch {
+	public static void main(String[] args) {
+		int idade = 18;
+		switch(idade) {
+		case 4: System.out.print("Nene"); break;
+		case 18: System.out.print("Adulto"); break;
+		}
+	}
+}
